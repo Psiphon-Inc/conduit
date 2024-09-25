a8a6df5fb8812d425d1bd146fad515bbb454ffcf
tmgrask <t.mackersy@psiphon.ca>
2024-09-25 12:06:49 -0400

    make layout more explicit

--- src/app/(app)/index.tsx
+++ src/app/(app)/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useWindowDimensions } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 import { ConduitOrbToggle } from "@/src/components/ConduitOrbToggle";
 import { ConduitSettings } from "@/src/components/ConduitSettings";
@@ -9,17 +10,30 @@ import { SafeAreaView } from "@/src/components/SafeAreaView";
 
 export default function HomeScreen() {
     const win = useWindowDimensions();
+    const insets = useSafeAreaInsets();
+
+    const totalUsableHeight = win.height - insets.top - insets.bottom;
+    const totalUsableWidth = win.width - insets.left - insets.right;
+    const logoWordmarkHeight = totalUsableHeight * 0.1;
+    // orb takes up a square with dimensions = width
+    // NOTE this assumes a portrait layout.
+    const conduitOrbToggleHeight = totalUsableWidth;
+    const conduitStatusHeight =
+        totalUsableHeight - logoWordmarkHeight - conduitOrbToggleHeight;
 
     return (
         <SafeAreaView>
             {/* Header takes up 10% of vertical space */}
-            <LogoWordmark width={win.width} height={win.height * 0.1} />
+            <LogoWordmark
+                width={totalUsableWidth}
+                height={logoWordmarkHeight}
+            />
             {/* Orb takes up a square, full width */}
-            <ConduitOrbToggle size={win.width} />
+            <ConduitOrbToggle size={conduitOrbToggleHeight} />
             {/* Status takes up the rest of the vertical space */}
             <ConduitStatus
-                width={win.width}
-                height={win.height * 0.9 - win.width}
+                width={totalUsableWidth}
+                height={conduitStatusHeight}
             />
             {/* Settings icon is absolutely positioned */}
             <ConduitSettings />
