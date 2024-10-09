be522745184651de39aae87498ef6309f7475cf1
tmgrask <t.mackersy@psiphon.ca>
2024-10-09 11:01:19 -0400

    put version in app.json; remove unused assets; simplify loading view

--- src/components/canvas/PsiphonConduitLoading.tsx
+++ src/components/canvas/PsiphonConduitLoading.tsx
@@ -29,28 +29,6 @@ export function PsiphonConduitLoading({
     size: number;
     opacity: SharedValue<number>;
 }) {
-    // draw Psiphon Conduit wordmark
-    const conduitSvg = useSVG(require("@/assets/images/conduit.svg"));
-    const psiphonSvg = useSVG(require("@/assets/images/psiphon.svg"));
-    const wordmarksOriginalWidth = 319;
-    const wordmarksOriginalHeight = 78;
-    const wordmarksTargetWidth = size; //100
-    const wordmarksTargetHeight =
-        (wordmarksTargetWidth / wordmarksOriginalWidth) *
-        wordmarksOriginalHeight;
-    const wordmarkSrc = rect(
-        0,
-        0,
-        wordmarksOriginalWidth,
-        wordmarksOriginalHeight,
-    );
-    const wordmarkDst = rect(
-        (size - wordmarksTargetWidth) / 2,
-        0,
-        wordmarksTargetWidth,
-        wordmarksTargetHeight,
-    );
-
     // animate the conduit flower logo pulsing with different colors
     const conduitFlowerSvg = useSVG(
         require("@/assets/images/conduit-flower-icon.svg"),
@@ -74,7 +52,7 @@ export function PsiphonConduitLoading({
     const flowerSrc = rect(0, 0, flowerOriginalDim, flowerOriginalDim);
     const flowerDst = rect(
         size / 2 - flowerSize / 2,
-        wordmarksTargetHeight * 2.2,
+        flowerSize / 2,
         flowerSize,
         flowerSize,
     );
@@ -85,7 +63,7 @@ export function PsiphonConduitLoading({
             1, 0, 0, 0, 0,
             0, 1, 0, 0, 0,
             0, 0, 1, 0, 0,
-            0, 0, 0, (lfo.value+0.5)*3, -0.2,
+            0, 0, 0, (lfo.value+0.5)*3, -0.3,
     ]
     });
 
@@ -117,22 +95,6 @@ export function PsiphonConduitLoading({
                 </Paint>
             }
         >
-            <Group transform={fitbox("contain", wordmarkSrc, wordmarkDst)}>
-                <ImageSVG
-                    svg={psiphonSvg}
-                    y={0}
-                    height={wordmarksOriginalHeight}
-                    width={wordmarksOriginalWidth}
-                />
-            </Group>
-            <Group transform={fitbox("contain", wordmarkSrc, wordmarkDst)}>
-                <ImageSVG
-                    svg={conduitSvg}
-                    y={wordmarksOriginalHeight}
-                    height={wordmarksOriginalHeight}
-                    width={wordmarksOriginalWidth}
-                />
-            </Group>
             <Group
                 layer={
                     <Paint>
