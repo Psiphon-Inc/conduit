1711adf5527febf24d3dec07a2a176ba5125d5cf
tmgrask <t.mackersy@psiphon.ca>
2024-10-09 12:52:06 -0400

    update privacy policy URL

--- src/constants.ts
+++ src/constants.ts
@@ -6,7 +6,8 @@ export const DEFAULT_INPROXY_LIMIT_BYTES_PER_SECOND = 10 * 1024 * 1024; // 10 MB
 export const INPROXY_MAX_CLIENTS_MAX = 25;
 export const INPROXY_MAX_MBPS_PER_PEER = 40;
 
-export const PRIVACY_POLICY_URL = "https://psiphon.ca/en/privacy.html";
+export const PRIVACY_POLICY_URL =
+    "https://psiphon-inc.github.io/conduit-privacy-policy/";
 
 // Hard code a common delay value for animations that fade in to wait until the
 // particle video is done playing.
