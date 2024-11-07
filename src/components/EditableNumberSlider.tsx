51f7f9794002d99965d8f7820795b3414e76b38f
tmgrask <t.mackersy@psiphon.ca>
2024-11-07 16:31:46 -0500

    use MB for default value, round to integer in number sliders

--- src/components/EditableNumberSlider.tsx
+++ src/components/EditableNumberSlider.tsx
@@ -66,7 +66,7 @@ export function EditableNumberSlider({
     const value = useSharedValue(originalValue);
     const displayText = useDerivedValue(() => {
         const changed = value.value === originalValue ? " " : "*";
-        return `${value.value}` + changed;
+        return `${Math.round(value.value)}` + changed;
     });
 
     const canvasSize = useSharedValue({ width: 0, height: 0 });
