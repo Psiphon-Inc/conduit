7d3dbb7007d8bd23c5ae4257a6b7af593af31538
Enda-O <e.omonahan@psiphon.ca>
2025-01-13 17:11:01 -0500

    [iOS] New unit test target with Swift Testing framework

---
+++ ios/conduitTests/conduitTests.swift
@@ -0,0 +1,15 @@
+//
+// Copyright (c) 2025, Psiphon Inc.
+// All rights reserved.
+//
+
+
+import Testing
+
+struct conduitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
