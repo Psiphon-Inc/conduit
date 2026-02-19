d1d87593bd50fc7aee2e0b741e0d1a2edc3ffeaf
tmgrask <t.mackersy@psiphon.ca>
2026-02-19 10:29:41 -0500

    ios tunnel-core 85311186

--- ios/PsiphonTunnel.xcframework/ios-arm64_x86_64-maccatalyst/PsiphonTunnel.framework/Versions/A/Headers/PsiphonTunnel.h
+++ ios/PsiphonTunnel.xcframework/ios-arm64_x86_64-maccatalyst/PsiphonTunnel.framework/Versions/A/Headers/PsiphonTunnel.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:181523b2f25d0f0479e06d8a4b20840333ca02dc299b7c84f04ee9df0883598c
-size 22293
+oid sha256:1e29c4e018e2e02bf28c100117392c5efc711fc76ef46292fd66ad4356f9299b
+size 23098
